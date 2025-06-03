2fe5383454615cdfe616065037568ce70abe697c
GabrielaShapiama28 <u202319448@upc.edu.pe>
2025-06-03 12:36:50 -0500

    chore: initial commit.

---
+++ src/main/java/com/restock/platform/RestockPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.restock.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestockPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestockPlatformApplication.class, args);
+    }
+
+}
