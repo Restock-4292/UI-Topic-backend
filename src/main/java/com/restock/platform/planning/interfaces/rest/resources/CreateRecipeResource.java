248492d5093b2dd53a895de6bcbb5f75b5d9946b
jahazielgg <u202319831@upc.edu.pe>
2025-06-22 02:48:35 -0500

    feat(recipes): add resources for adding and updating recipes and supplies

--- src/main/java/com/restock/platform/planning/interfaces/rest/resources/CreateRecipeResource.java
+++ src/main/java/com/restock/platform/planning/interfaces/rest/resources/CreateRecipeResource.java
@@ -10,12 +10,7 @@ public record CreateRecipeResource(
         int userId,
         List<RecipeSupplyResource> supplies
 ) {
-    public CreateRecipeResource{
-        if (name == null || name.isBlank()) throw new IllegalArgumentException("Name cannot be null or blank");
-        if (description == null || description.isBlank()) throw new IllegalArgumentException("Description cannot be null or blank");
-        if (imageUrl == null || imageUrl.isBlank()) throw new IllegalArgumentException("Image URL cannot be null or blank");
-        if (totalPrice == null || totalPrice < 0) throw new IllegalArgumentException("Total price must be a non-negative number");
-        if (userId <= 0) throw new IllegalArgumentException("User ID must be a positive number");
-        if (supplies == null || supplies.isEmpty()) throw new IllegalArgumentException("Supplies cannot be null or empty");
+    public List<RecipeSupplyResource> getSuppliesOrEmpty() {
+        return supplies == null ? List.of() : supplies;
     }
 }
