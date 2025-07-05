357912334e40af9b5b779afb5f88d64313848800
Julio Castro Alejos <110569049+JulioXC4@users.noreply.github.com>
2025-07-04 22:10:34 -0500

    Update CorsConfigProd.java

--- src/main/java/com/restock/platform/shared/infrastructure/configuration/cors/CorsConfigProd.java
+++ src/main/java/com/restock/platform/shared/infrastructure/configuration/cors/CorsConfigProd.java
@@ -11,7 +11,7 @@ public class CorsConfigProd implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*") // Replace with our production frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
